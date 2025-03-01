54025aa6fab426e0b18730705b9260e6231bb2cc
gjohnn <juansgb316@outlook.com>
2025-03-01 20:23:35 -0300

    movement with hjkl on insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,21 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Buscar archivos con Telescope
+map("n", "<leader>f", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+
+-- Duplicar línea actual en modo insert con Ctrl+D
+map("i", "<C-d>", "<Esc>yypA", { noremap = true, silent = true })
+
+-- Guardar archivo con Ctrl+S en normal, insert y visual
+map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>", { noremap = true, silent = true })
+
+-- Movimiento en modo insert con Alt + h/j/k/l
+map("i", "<A-h>", "<Left>", { noremap = true, silent = true })
+map("i", "<A-l>", "<Right>", { noremap = true, silent = true })
+map("i", "<A-j>", "<Down>", { noremap = true, silent = true })
+map("i", "<A-k>", "<Up>", { noremap = true, silent = true })
