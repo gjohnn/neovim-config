01cee32ea3706a7dff50ff4816bb1523cc1f1348
Juan Guerrero <juansgb316@outlook>
2025-04-06 13:47:46 -0300

    Transparency

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ require("config.lazy")
 require("plugins.dashboard")
 -- Configuración para vim-lens
 vim.g.lens_enabled = 1
+-- Activa el fondo transparente en Neovim
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE") -- Para que el fondo sea transparente en todas las ventanas
+vim.cmd("hi NonText guibg=NONE ctermbg=NONE") -- Opcional, para quitar fondo en caracteres no visibles
+vim.cmd("hi VertSplit guibg=NONE ctermbg=NONE") -- Para las líneas divisorias
+vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE") -- Para la barra de estado
